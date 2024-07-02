0a1001b41cb17bb5d650691c9afaf4ad35f746c4
Seisuke Fukuda <razerrmc163@gmail.com>
2024-07-02 23:08:17 +0900

    Finished Controller methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning seed"
+Restaurant.destroy_all
+
+puts "Seeding"
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Address.street_name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+end
+
+puts "Creating #{Restaurant.name}"
+
+puts "Finished"
